006b80ec6969b36482cfc092f44ebf8094f98701
Salsicha1001 <felumais14@gmail.com>
2022-03-24 00:11:09 -0300

    ajuste load

--- src/main/java/com/ecommerceback/Service/Cards/CardService.java
+++ src/main/java/com/ecommerceback/Service/Cards/CardService.java
@@ -48,6 +48,9 @@ public class CardService {
             addLanguage(language);
             String url = url_language_base +url_offset+offset.toString()+url_num+num.toString();
             CardsResponse result = restTemplate.getForObject(url, CardsResponse.class);
+            if(language.equals("pt")){
+                result = convertPt(result);
+            }
             return new ResponseEntity(result,HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
@@ -55,13 +58,16 @@ public class CardService {
         }
     }
 
-    public ResponseEntity<Page<CardsResponse>> getSearchCard(Integer offset, Integer num,String language,String race,String type,
+    public ResponseEntity<CardsResponse> getSearchCard(Integer offset, Integer num,String language,String race,String type,
                                                              String archetype, String attribute,String level,
                                                              String fname,String def,String atk){
         try{
             addLanguage(language);
             String url = createUrl(race,type,archetype,attribute,level,fname,def,atk, offset, num);;
             CardsResponse result = restTemplate.getForObject(url, CardsResponse.class);
+            if(language.equals("pt")){
+                result = convertPt(result);
+            }
             return new ResponseEntity(result,HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
@@ -146,6 +152,9 @@ public class CardService {
         try{
             String url = url_language_base+"&name="+name;
             CardsResponse result = restTemplate.getForObject(url, CardsResponse.class);
+            if(language.equals("pt")){
+            result = convertPt(result);
+            }
             return new ResponseEntity(result,HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
@@ -157,6 +166,9 @@ public class CardService {
         try{
             String url = url_language_base+"&id="+id;
             CardsResponse result = restTemplate.getForObject(url, CardsResponse.class);
+            if(language.equals("pt")){
+                result = convertPt(result);
+            }
             return new ResponseEntity(result,HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
@@ -169,7 +181,9 @@ public class CardService {
             addLanguage(language);
             String url = url_language_base +url_offset+offset.toString()+url_num+num.toString();
             CardsResponse result = restTemplate.getForObject(url, CardsResponse.class);
+
             CardsResumeDtoResponse cards = convertToResumeDTO(result);
+
             return new ResponseEntity(cards,HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
@@ -188,5 +202,246 @@ public class CardService {
         }
         result.setData(tmp);
         return result;
+    }
+
+
+
+
+    private CardsResponse convertPt(CardsResponse cardsResponse){
+        CardsResponse response = new CardsResponse();
+        response.setMeta(cardsResponse.getMeta());
+        List<CardsDtoResponse> tmp = new ArrayList<>();
+        for(CardsDtoResponse res:cardsResponse.getData()){
+            res.setRace(setPtRace(res.getRace()));
+            res.setType(setPtType(res.getType()));
+            res.setAttribute(setPtAtribute(res.getAttribute()));
+            tmp.add(res);
+        }
+        response.setData(tmp);
+        return response;
+    }
+
+
+    private String setPtRace(String msg){
+        if(msg!=null) {
+
+            switch (msg) {
+                    case "Aqua":
+                        msg = "Água";
+                        break;
+                    case "Beast":
+                        msg = "Fera";
+                        break;
+                    case "Beast-Warrior":
+                        msg = "Besta-Guerreira";
+                        break;
+                    case "Creator-God":
+                        msg = "Deus-Criador";
+                        break;
+                    case "Cyberse":
+                        msg = "Cyberso";
+                        break;
+                    case "Dinosaur":
+                        msg = "Dinossauro";
+                        break;
+                    case "Fairy":
+                        msg = "Fada";
+                        break;
+                    case "Divine-Beast":
+                        msg = "Besta Divina";
+                        break;
+                    case "Dragon":
+                        msg = "Dragão";
+                        break;
+                    case "Fiend":
+                        msg = "Demônio";
+                        break;
+                    case "Fish":
+                        msg = "Peixe";
+                        break;
+                    case "Insect":
+                        msg = "Inseto";
+                        break;
+                    case "Machine":
+                        msg = "Máquina";
+                        break;
+                    case "Plant":
+                        msg = "Plantar";
+                        break;
+                    case "Psychic":
+                        msg = "Psíquico";
+                        break;
+                    case "Pyro":
+                        msg = "Pyro";
+                        break;
+                    case "Reptile":
+                        msg = "Réptil";
+                        break;
+                    case "Rock":
+                        msg = "Rocha";
+                        break;
+                    case "Sea Serpent":
+                        msg = "Serpente do mar";
+                        break;
+                    case "Spellcaster":
+                        msg = "Feiticeiro";
+                        break;
+                    case "Thunder":
+                        msg = "Trovão";
+                        break;
+                    case "Warrior":
+                        msg = "Guerreiro";
+                        break;
+                    case "Winged Beast":
+                        msg = "Besta Alada";
+                        break;
+                    case "Normal":
+                        msg = "Normal";
+                        break;
+                    case "Field":
+                        msg = "Campo";
+                        break;
+                    case "Equip":
+                        msg = "Equipamento";
+                        break;
+                    case "Continuous":
+                        msg = "Contínuo";
+                        break;
+                    case "Quick-Play":
+                        msg = "Magia Rápida";
+                        break;
+                    case "Ritual":
+                        msg = "Ritual";
+                        break;
+                    case "Counter":
+                        msg = "Resposta";
+                        break;
+                    default:
+                        break;
+                }
+            }
+           return msg;
+    }
+    private String setPtType(String msg){
+        if(msg!=null) {
+
+            switch (msg) {
+                case "Effect Monster":
+                    msg = "Monstro de Efeito";
+                    break;
+                case "Flip Effect Monster":
+                    msg = "Monstro Efeito Virar";
+                    break;
+                case "Flip Tuner Effect Monster":
+                    msg = "Monstro de Efeito Flip Tuner";
+                    break;
+                case "Gemini Monster":
+                    msg = "Monstro de Gêmeos";
+                    break;
+                case "Normal Monster":
+                    msg = "Monstro Normal";
+                    break;
+                case "Normal Tuner Monster":
+                    msg = "Monstro Regulador Normal";
+                    break;
+                case "Pendulum Effect Monster":
+                    msg = "Monstro Efeito Pêndulo";
+                    break;
+                case "Pendulum Flip Effect Monster":
+                    msg = "Monstro Efeito Pêndulo Virar";
+                    break;
+                case "Pendulum Normal Monster":
+                    msg = "Monstro Normal Pêndulo";
+                    break;
+                case "Pendulum Tuner Effect Monster":
+                    msg = "Monstro Efeito Regulador Pêndulo";
+                    break;
+                case "Ritual Effect Monster":
+                    msg = "Monstro de Efeito Ritual";
+                    break;
+                case "Ritual Monster":
+                    msg = "Monstro Ritual";
+                    break;
+                case "Skill Card":
+                    msg = "Cartão de Habilidade";
+                    break;
+                case "Spell Card":
+                    msg = "Carta de Magia";
+                    break;
+                case "Spirit Monster":
+                    msg = "Monstro Espiritual";
+                    break;
+                case "Toon Monster":
+                    msg = "Monstro Toon";
+                    break;
+                case "Trap Card":
+                    msg = "Carta Armadilha";
+                    break;
+                case "Tuner Monster":
+                    msg = "Monstro Regulador";
+                    break;
+                case "Union Effect Monster":
+                    msg = "Monstro Efeito União";
+                    break;
+                case "Fusion Monster":
+                    msg = "Monstro de Fusão";
+                    break;
+                case "Link Monster":
+                    msg = "Monstro Link";
+                    break;
+                case "Pendulum Effect Fusion Monster":
+                    msg = "Monstro de Fusão Efeito Pêndulo";
+                    break;
+                case "Synchro Monster":
+                    msg = "Monstro Sincro";
+                    break;
+                case "Synchro Pendulum Effect Monster":
+                    msg = "Monstro Efeito Pêndulo Sincro";
+                    break;
+                case "Synchro Tuner Monster":
+                    msg = "Monstro Regulador Sincro";
+                    break;
+                case "XYZ Monster":
+                    msg = "Monstro XYZ";
+                    break;
+                case "XYZ Pendulum Effect Monster":
+                    msg = "Monstro de Efeito Pêndulo XYZ";
+                    break;
+                default:
+                    break;
+            }
+        }
+        return msg;
+    }
+    private String setPtAtribute(String msg){
+        if(msg!=null) {
+            switch (msg) {
+                case "DARK":
+                    msg = "Trevas";
+                    break;
+                case "EARTH":
+                    msg = "Terra";
+                    break;
+                case "FIRE":
+                    msg = "Fogo";
+                    break;
+                case "LIGHT":
+                    msg = "Luz";
+                    break;
+                case "WATER":
+                    msg = "Água";
+                    break;
+                case "WIND":
+                    msg = "Vento";
+                    break;
+                case "DIVINE":
+                    msg = "Divino";
+                    break;
+                default:
+                    msg = null;
+                    break;
+            }
+        }
+        return msg;
     }
 }
