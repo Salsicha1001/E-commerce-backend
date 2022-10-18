936dcf075cc9af93e819906a71a54888abb30023
juniorkiobr <juniorkiolouco66@gmail.com>
2022-10-18 16:21:17 -0300

    Adicionando CrossOrigin

--- src/main/java/com/ecommerceback/controller/CardsController.java
+++ src/main/java/com/ecommerceback/controller/CardsController.java
@@ -2,14 +2,12 @@ package com.ecommerceback.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ecommerceback.service.impl.CardServiceImpl;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/cards")
 public class CardsController {
     @Autowired
