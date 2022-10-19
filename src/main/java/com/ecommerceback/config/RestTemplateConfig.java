c299ed746d20fc0253bd19066963225bfcbdffa1
juniorkiobr <juniorkiolouco66@gmail.com>
2022-10-19 14:03:04 -0300

    Criando filtro para o cors

--- src/main/java/com/ecommerceback/config/RestTemplateConfig.java
+++ src/main/java/com/ecommerceback/config/RestTemplateConfig.java
@@ -1,11 +1,21 @@
 package com.ecommerceback.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class RestTemplateConfig {
+public class RestTemplateConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    CorsInterceptor corsInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(corsInterceptor);
+    }
 
 }
